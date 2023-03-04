53c5a5ef6ea826ba5b5f8f713d3a02576b155b53
leandro couretot <leandrocouretot@leandros-MacBook-Air.local>
2023-03-04 10:39:06 -0300

    prueba de agregar numeros al navbar

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -9,7 +9,7 @@ const LoggedInLinks = () => {
     return (
         <>
             <li className="nav-item">
-                <Link className="nav-link active" to="/">Home</Link>
+                <Link className="nav-link active" to="/">Home2</Link>
             </li>
             <li className="nav-item">
                 <Link className="nav-link  active" to="/create_recipe">Create Recipes</Link>
@@ -26,7 +26,7 @@ const LoggedOutLinks = () => {
     return (
         <>
             <li className="nav-item">
-                <Link className="nav-link active" to="/">Home</Link>
+                <Link className="nav-link active" to="/">Home3</Link>
             </li>
             <li className="nav-item">
                 <Link className="nav-link active" to="/signup">Sign Up</Link>
@@ -46,7 +46,7 @@ const NavBar = () => {
     return (
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
             <div className="container-fluid">
-                <Link className="navbar-brand" to="/">Recipes</Link>
+                <Link className="navbar-brand" to="/">Recipes4</Link>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
